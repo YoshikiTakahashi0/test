bca34401b230eff3e40c50451ddbe44bec480f39
YoshikiTakahashi0 <yoshi.main00@gmail.com>
2022-08-27 16:14:23 +0900

    8_4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,13 +8,14 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='create'>[<a href='/posts/create'>create</a>]</p>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'>
                         <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                     </h2>
-                    <p class='body'>This is a sample body.</p>
+                    <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
