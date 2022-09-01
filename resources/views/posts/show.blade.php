43cb51a6f2b81b7c19d7e08b7359b3214a6bddda
YoshikiTakahashi0 <yoshi.main00@gmail.com>
2022-09-01 16:46:16 +0900

    08_6complete

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,6 +9,10 @@
     <body>
         <h1>Blog Name</h1>
         <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+            @csrf
+            @method('DELETE')
+        <button type="submit">delete</button>
         <div class='post'>
                 <h2 class='title'>{{ $post->title }}</h2>
                 <p class='body'>{{ $post->body }}</p>
